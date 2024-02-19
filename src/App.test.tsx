9ab641138d9418dd06a02b8d2ee23afcf786dfd3
Victor Fuentes <victorp.fuentes@gmail.com>
2024-02-18 20:54:25 -0600

    remove junk test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,4 @@
-import React from 'react';
-import {render, screen,} from '@testing-library/react';
-import App from './App';
-import '@testing-library/jest-dom';
-
-test('Basic Navigation Exists', () => {
-    render(<App/>);
-    expect(screen.getByText(/Basic Navigation/i)).toBeInTheDocument();
-});
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import "@testing-library/jest-dom";
